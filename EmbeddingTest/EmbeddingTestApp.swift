634315937e38130487d0ff2c2b2131719d20ae68
Simon Liang <simon@x-tech.io>
2025-05-01 21:18:06 +0800

    Initial Commit

---
+++ EmbeddingTest/EmbeddingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbeddingTestApp.swift
+//  EmbeddingTest
+//
+//  Created by Simon Liang on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct EmbeddingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
